943f47cd8ce8f9355036e058c9dfdf9cc329dd46
Karol Depka Pradzinski <kd@kd>
2016-11-09 00:41:42 +0100

    Book2Action app skeleton

---
+++ Book2Action/app/src/main/java/book2action/com/book2action/SelectInterestsActivity.java
@@ -0,0 +1,13 @@
+package book2action.com.book2action;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectInterestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_interests);
+    }
+}
